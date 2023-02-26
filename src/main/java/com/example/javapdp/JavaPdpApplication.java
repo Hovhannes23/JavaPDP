bde7d5df6a258741916859110bb0099431869f27
Ovannes Shaumyan <ovannes_shaumyan@senla.eu>
2023-02-26 15:56:41 +0300

    add DeadLockExample

---
+++ src/main/java/com/example/javapdp/JavaPdpApplication.java
@@ -0,0 +1,13 @@
+package com.example.javapdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPdpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javapdp/JavaPdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javapdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPdpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
